7cf183e35835ff4415025a50332f5e8ca95452ab
Rachel4858 <rachel458458@gmail.com>
2020-03-12 15:28:01 +0900

    Creating TodoList section - html and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import style from './App.css';
 
-function App() {
-  return (
-    <div className="App" />
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div id="todo-list-section">
+        <div id="title">TodoList</div>
+        <div id="list-wrapper">
+            <div class="item">schedule 1</div>
+            <div class="item">schedule 2</div>
+            <div class="item">schedule 3</div>
+          </div>
+        <div id="add-item">+ 새 카드 추가하기</div>
+      </div>
+    );
+  }
 }
 
 export default App;
